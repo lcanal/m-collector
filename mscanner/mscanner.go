6b9d8a5f9fe5c30665a01326e4f9215250c580e1
Luis Canales <cluchomucho@gmail.com>
2018-07-31 16:21:32 -0700

    Testing file walk path

--- mscanner/mscanner.go
+++ mscanner/mscanner.go
@@ -1,10 +1,18 @@
 package mscanner
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 )
 
 //ScanModules scan the node_modules directory
 func ScanModules() {
-	log.Printf("ERR!!!!!!!!!!!!")
+	files, err := ioutil.ReadDir("node_modules")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
